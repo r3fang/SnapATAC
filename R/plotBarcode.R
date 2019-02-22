103037daa941f96c88dcd2fca954b5e60d9cae88
r3fang <r3fang@ucsd.edu>
2019-02-21 17:16:31 -0800

    add Sst motif URL

--- R/plotBarcode.R
+++ R/plotBarcode.R
@@ -20,11 +20,11 @@ plotBarcode.default <- function(obj){
 	if(nrow(obj@metaData) == 0){stop(paste("Error @plotBarcode: obj@metaData is empty!", sep=""))}
 	barcode = getMetaData(obj);
 	par(mfrow=c(3,2));
-	hist(log(barcode$TN + 1,10), col="grey", main="log10(Total Fragments)", xlab="log10(Total Fragments + 1)", breaks=50);
-	hist(log(barcode$UQ + 1,10), col="grey", main="log10(UMI)", xlab="log10(UMI + 1)", breaks=50);
-	hist((barcode$UM+1)/(barcode$TN+1), col="grey", main="Mappability Ratio", xlab="Mappability", breaks=50, xlim=c(0, 1));
-	hist((barcode$PP+1)/(barcode$UM+1), col="grey", main="Proper Paired Ratio", xlab="Proper Paired", breaks=50, xlim=c(0, 1));
-	hist(1 - (barcode$UQ+1)/(barcode$PP+1), col="grey", main="Duplicate Rate Ratio", xlab="Duplicate Rate", breaks=50, xlim=c(0, 1));
-	hist((barcode$CM+1) / (barcode$UQ+1), col="grey", main="chrM Rate Ratio", xlab="chrM Rate", breaks=50, xlim=c(0, 1));
+	hist(log(barcode$TN + 1,10), col="grey", main="log10(Total Fragments)", xlab="log10(Total Fragments + 1)", breaks=50, border="grey");
+	hist(log(barcode$UQ + 1,10), col="grey", main="log10(UMI)", xlab="log10(UMI + 1)", breaks=50, border="grey");
+	hist((barcode$UM+1)/(barcode$TN+1), col="grey", main="Mappability Ratio", xlab="Mappability", breaks=50, xlim=c(0, 1), border="grey");
+	hist((barcode$PP+1)/(barcode$UM+1), col="grey", main="Proper Paired Ratio", xlab="Proper Paired", breaks=50, xlim=c(0, 1), border="grey");
+	hist(1 - (barcode$UQ+1)/(barcode$PP+1), col="grey", main="Duplicate Rate Ratio", xlab="Duplicate Rate", breaks=50, xlim=c(0, 1), border="grey");
+	hist((barcode$CM+1) / (barcode$UQ+1), col="grey", main="chrM Rate Ratio", xlab="chrM Rate", breaks=50, xlim=c(0, 1),  border="grey");
 }
 
